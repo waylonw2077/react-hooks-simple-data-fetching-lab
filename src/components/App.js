1e797c194633d6a800ebb8342d2cf3a54fa1d829
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-04-14 11:27:04 -0400

    completes assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setDogImage(data.message);
+      })
+      .catch(error => {
+        console.error('Error fetching dog image:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {!dogImage && <p>Loading...</p>}
+      {dogImage && <img src={dogImage} alt="A Random Dog" />}
+    </div>
+  );
+}
+
+export default App;
